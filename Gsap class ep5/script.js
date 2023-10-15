42c90251b5d62183f68407b08c2c034d0c26df2d
sahilmoktan <moktanshail9@gmail.com>
2023-10-15 19:23:51 +0545

    gsap ep5 font imported

---
+++ Gsap class ep5/script.js
@@ -0,0 +1,49 @@
+var tl = gsap.timeline()
+
+tl.from('#nav',{
+    
+    opacity:0,
+})
+
+
+
+
+
+// tl.from('#nav h1, #nav h4, #nav h3 ',{
+//     y:-80,
+//     duration:0.5,
+//     delay:0.5,
+//     opacity:0,
+//     stagger:0.2,
+//     // rotate:360,
+    
+// })
+
+
+// tl.from('#left h1',{
+// x:-500,
+// opacity:0,
+// duration:0.8,
+// stagger:0.3
+// })
+
+// tl.from('#right img',{
+// scale:2,
+// opacity:0,
+// duration:0.8
+
+// })
+
+// gsap.from('#page2 #box',{
+// scale:0,
+// opacity:0,
+// duration:1,
+// stagger:0.3,
+// scrollTrigger:{
+//     trigger:"#page2 #box",
+//     scroller:'body',
+//     // markers:true,
+//     start: 'top 70%'
+    
+// }
+// })
